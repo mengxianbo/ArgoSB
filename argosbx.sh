86d503091cba0446c2cda4356394ddef7677f4a7
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-11-19 10:14:33 +0800

    Update argosbx.sh

--- argosbx.sh
+++ argosbx.sh
@@ -75,7 +75,7 @@ v4dq=$( (command -v curl >/dev/null 2>&1 && curl -s4m5 -k https://ip.fm | sed -E
 v6dq=$( (command -v curl >/dev/null 2>&1 && curl -s6m5 -k https://ip.fm | sed -E 's/.*Location: ([^,]+(, [^,]+)*),.*/\1/' 2>/dev/null) || (command -v wget >/dev/null 2>&1 && timeout 3 wget -6 --tries=2 -qO- https://ip.fm | grep '<span class="has-text-grey-light">Location:' | tail -n1 | sed -E 's/.*>Location: <\/span>([^<]+)<.*/\1/' 2>/dev/null) )
 }
 warpsx(){
-warpurl=$(curl -s --max-time 6 https://warp.renky.eu.org)
+warpurl=$(curl -s --max-time 6 https://ygkkk-warp.renky.eu.org)
 if echo "$warpurl" | grep -q ygkkk; then
 pvk=$(echo "$warpurl" | awk -F'：' '/Private_key/{print $2}' | xargs)
 wpv6=$(echo "$warpurl" | awk -F'：' '/IPV6/{print $2}' | xargs)
